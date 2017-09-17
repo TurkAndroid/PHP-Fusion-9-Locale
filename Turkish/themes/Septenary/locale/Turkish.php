2e1d5029d94915f47cae847d613ec41ad9be37c6
Türk Android <alper.peri@gmail.com>
2017-09-17 21:20:36 +0300

    Update Turkish.php

--- Turkish/themes/Septenary/locale/Turkish.php
+++ Turkish/themes/Septenary/locale/Turkish.php
@@ -1,12 +1,15 @@
 <?php
-$locale['sept_001'] = "Login";
-$locale['sept_002'] = "Register";
-$locale['sept_003'] = "Admin";
-$locale['sept_004'] = "My Profile";
-$locale['sept_005'] = "Logout";
-$locale['sept_006'] = "Search...";
-$locale['sept_007'] = "News";
-$locale['sept_008'] = "Gallery";
+/**
+ * @Translated by MAxwELL_TR Web Site (https://alperperi.com)
+ */
+$locale['sept_001'] = "Oturum Aç";
+$locale['sept_002'] = "Üye Ol";
+$locale['sept_003'] = "Yönetici";
+$locale['sept_004'] = "Profilim";
+$locale['sept_005'] = "Çıkış";
+$locale['sept_006'] = "Ara...";
+$locale['sept_007'] = "Haberler";
+$locale['sept_008'] = "Galeri";
 $locale['sept_009'] = "Forum";
-$locale['sept_010'] = "Downloads";
-$locale['sept_011'] = "Made on HTML5 and CSS3";
+$locale['sept_010'] = "İndirmeler";
+$locale['sept_011'] = "HTML5 ve CSS3 üzerinde yapılmıştır";
