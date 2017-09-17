890f7acb2fd0343d40f314c5293e2733bdbf49d8
Türk Android <alper.peri@gmail.com>
2017-09-17 21:23:06 +0300

    Update Turkish.php

--- Turkish/themes/admin_themes/Artemis/locale/Turkish.php
+++ Turkish/themes/admin_themes/Artemis/locale/Turkish.php
@@ -1,8 +1,11 @@
 <?php
-$locale['spotlight'] = "Spotlight";
-$locale['spotlight_search'] = "Search for Apps";
-$locale['copyright'] = "Copyright &copy; ";
-$locale['render_engine'] = "Render Engine";
+/**
+ * @Translated by MAxwELL_TR Web Site (https://alperperi.com)
+ */
+$locale['spotlight'] = "Spot";
+$locale['spotlight_search'] = "Uygulamalarda Ara";
+$locale['copyright'] = "Telif hakkı &copy; ";
+$locale['render_engine'] = "Motor Oluşumu";
 $locale['artemis_admin'] = "Artemis PHP-Fusion 9";
-$locale['admin_apps'] = "Apps";
-$locale['admin_collapse'] = "Collapse Menu";
+$locale['admin_apps'] = "Uygulamalar";
+$locale['admin_collapse'] = "Menüyü Daralt";
