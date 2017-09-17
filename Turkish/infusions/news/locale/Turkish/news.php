9ddeefcd2a72bb3bc3edec804f3573f77b6570ee
Türk Android <alper.peri@gmail.com>
2017-09-17 21:08:50 +0300

    Update news.php

--- Turkish/infusions/news/locale/Turkish/news.php
+++ Turkish/infusions/news/locale/Turkish/news.php
@@ -1,5 +1,5 @@
 <?php
-$locale['news_0001'] = "Okumaya Devam Et";
+$locale['news_0001'] = "Devamını Oku";
 $locale['news_0002'] = "Yazdır";
 $locale['news_0003'] = "Düzenle";
 $locale['news_0004'] = "Haber";
@@ -8,12 +8,12 @@ $locale['news_0006'] = "Kategorilenmedi";
 $locale['news_0008'] = "Son Güncellenme";
 $locale['news_0009'] = "Haber Kategorileri";
 $locale['news_0010'] = "Tüm Diğer Kategoriler";
-$locale['news_0011'] = "En Son Haber";
-$locale['news_0012'] = "En Çok Yorumlanan Haber";
-$locale['news_0013'] = "En Çok Oylanan Haber";
-$locale['news_0014'] = "Thumb view";
+$locale['news_0011'] = "En Son Haberler";
+$locale['news_0012'] = "En Çok Yorumlananlar";
+$locale['news_0013'] = "En Çok Oylananlar";
+$locale['news_0014'] = "Galeri Görünümü";
 $locale['news_0015'] = "Liste görünümü";
-$locale['news_0016'] = "Hiçbir kategori tanımlanmadı.";
-$locale['news_0017'] = "Filter by";
+$locale['news_0016'] = "Tanımlanmış Haber Kategorisi Yok.";
+$locale['news_0017'] = "Filtrele";
 $locale['news_0018'] = "Tüm haberleri göster";
 $locale['news_0019'] = "Haber Galerisi";
