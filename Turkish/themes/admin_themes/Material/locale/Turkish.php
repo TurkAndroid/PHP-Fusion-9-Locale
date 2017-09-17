5bebcf067e126789b85d5aabc33e7ab3e066ef59
Türk Android <alper.peri@gmail.com>
2017-09-17 21:27:39 +0300

    Update Turkish.php

--- Turkish/themes/admin_themes/Material/locale/Turkish.php
+++ Turkish/themes/admin_themes/Material/locale/Turkish.php
@@ -15,21 +15,24 @@
 | copyright header is strictly prohibited without
 | written permission from the original author(s).
 +--------------------------------------------------------*/
-$locale['material_001'] = 'Search (more than 2 characters)';
-$locale['material_002'] = 'Theme Settings';
-$locale['material_003'] = 'Hide Sidebar';
-$locale['material_004'] = 'Small Sidebar';
-$locale['material_005'] = 'Fixed Menu';
-$locale['material_006'] = 'Fixed Sidebar';
-$locale['material_007'] = 'Fixed Footer Errors';
-$locale['material_008'] = 'Full Screen';
-$locale['material_009'] = 'Messages';
-$locale['material_010'] = 'Read';
-$locale['material_011'] = 'New Message';
-$locale['material_012'] = 'There are no messages';
+/**
+ * @Translated by MAxwELL_TR Web Site (https://alperperi.com)
+ */
+$locale['material_001'] = 'Ara (2 karakterden fazla)';
+$locale['material_002'] = 'Tema ayarları';
+$locale['material_003'] = 'Kenar Çubuğunu Gizle';
+$locale['material_004'] = 'Kenar Çubuğunu Küçült';
+$locale['material_005'] = 'Sabit Menü';
+$locale['material_006'] = 'Sabit Kenar Çubuğu';
+$locale['material_007'] = 'Sabit Altbilgi Hataları';
+$locale['material_008'] = 'Tam Ekran';
+$locale['material_009'] = 'Mesajlar';
+$locale['material_010'] = 'Okuma';
+$locale['material_011'] = 'Yeni Mesaj';
+$locale['material_012'] = 'Mesaj yok';
 // Result Messages
-$locale['material_100'] = 'Failed system validation';
-$locale['material_101'] = 'Failed system security measures';
-$locale['material_102'] = 'Pages failed to load';
-$locale['material_103'] = 'Search string is too short';
-$locale['material_104'] = 'There are no results found';
+$locale['material_100'] = 'Başarısız sistem doğrulaması';
+$locale['material_101'] = 'Başarısız sistem güvenlik önlemleri';
+$locale['material_102'] = 'Sayfa yüklenemedi';
+$locale['material_103'] = 'Arama dizesi çok kısa';
+$locale['material_104'] = 'Sonuç bulunamadı';
