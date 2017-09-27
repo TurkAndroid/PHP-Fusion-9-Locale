62aa75cac63f19c91f5157b40d2e5e3bf833b95e
Türk Android <alper.peri@gmail.com>
2017-09-27 21:27:48 +0300

    Update comments.php

--- Turkish/locale/Turkish/admin/comments.php
+++ Turkish/locale/Turkish/admin/comments.php
@@ -1,16 +1,19 @@
 <?php
+/**
+ * @Translated by MAxWELL_TR Web Site(https://alperperi.com)
+ */
 // Comments titles
-$locale['400'] = "Edit Comment";
-$locale['401'] = "Comments";
+$locale['400'] = "Yorum Düzenle";
+$locale['401'] = "Yorumlar";
 // Comments messages
-$locale['410'] = "Comment updated";
-$locale['411'] = "Comment deleted";
+$locale['410'] = "Yorum güncellendi";
+$locale['411'] = "Yorum silindi";
 // Edit Comment form
-$locale['420'] = "Disable smileys in this comment";
-$locale['421'] = "Save Comment";
+$locale['420'] = "Bu yorumdaki yüz ifadelerini devre dışı bırak";
+$locale['421'] = "Yorum Kaydet";
 // Comments
-$locale['430'] = "Edit";
-$locale['431'] = "Delete";
-$locale['432'] = "User IP:";
-$locale['433'] = "Delete this comment?";
-$locale['434'] = "There are no comments for this item.";
+$locale['430'] = "Düzenle";
+$locale['431'] = "Sil";
+$locale['432'] = "Kullanıcı IP:";
+$locale['433'] = "Bu yorum silinsin mi?";
+$locale['434'] = "Bu öğe için yorum bulunmuyor.";
