fc1e6deda672eeb482a8f1f65a7f0b7a0ea3e5ce
Türk Android <alper.peri@gmail.com>
2017-09-27 21:14:30 +0300

    Update bbcodes.php

--- Turkish/locale/Turkish/admin/bbcodes.php
+++ Turkish/locale/Turkish/admin/bbcodes.php
@@ -1,24 +1,27 @@
 <?php
-$locale['400'] = "BB Codes Admin";
-$locale['401'] = "Test BB Codes";
+/**
+ * @Translated by MAxWELL_TR Web Site(https://alperperi.com)
+ */
+$locale['400'] = "BB Kodları Yöneticisi";
+$locale['401'] = "BB Kodları Testi";
 // Enabled BB Codes
-$locale['402'] = "Enabled BB Codes";
-$locale['403'] = "Name";
-$locale['404'] = "Icon";
-$locale['405'] = "Description";
-$locale['406'] = "Usage";
-$locale['407'] = "Order";
-$locale['408'] = "Down";
-$locale['409'] = "Up";
-$locale['410'] = "Disable";
-$locale['411'] = "There are no BB Codes enabled.";
+$locale['402'] = "BB Kodları Aktif et";
+$locale['403'] = "Adı";
+$locale['404'] = "İkon";
+$locale['405'] = "Açıklama";
+$locale['406'] = "Kullanım";
+$locale['407'] = "Sıralama";
+$locale['408'] = "Aşağı";
+$locale['409'] = "Yukarı";
+$locale['410'] = "Kapalı";
+$locale['411'] = "Etkin BB Kodu yok.";
 // Disabled BB Codes
-$locale['413'] = "Disabled BB Codes";
-$locale['414'] = "Enable";
-$locale['415'] = "Line break";
-$locale['416'] = "There are no BB Codes disabled.";
+$locale['413'] = "Kapalı BB Kodları";
+$locale['414'] = "Aktif et";
+$locale['415'] = "Satır sonu";
+$locale['416'] = "Kapatılmış BB Kodları yok.";
 // Test BB Codes
-$locale['417'] = "BB Codes Test Output";
-$locale['418'] = "Disable smileys in this test";
-$locale['418a'] = "Test Message";
-$locale['418b'] = "Please enter a message to test BB Codes.";
+$locale['417'] = "BB Kodları Test Çıktısı";
+$locale['418'] = "Bu testte iafeleri devre dışı bırak";
+$locale['418a'] = "Test Mesajı";
+$locale['418b'] = "Lütfen BB Kodlarını test etmek için bir mesaj girin.";
