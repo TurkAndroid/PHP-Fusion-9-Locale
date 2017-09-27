71f637271cd02d5412f3c3685ae301d70f378819
Türk Android <alper.peri@gmail.com>
2017-09-27 22:19:44 +0300

    Update image_uploads.php

--- Turkish/locale/Turkish/admin/image_uploads.php
+++ Turkish/locale/Turkish/admin/image_uploads.php
@@ -1,29 +1,31 @@
 <?php
+/**
+ * @Translated by mert25 Web Site http://amatorce.net/
 // Delete Image
-$locale['400'] = "Delete Image";
-$locale['401'] = "Image Deleted";
-$locale['402'] = "Return to Images Admin";
-$locale['403'] = "Return to Admin Index";
+$locale['400'] = "Resim Sil";
+$locale['401'] = "Resim Silindi";
+$locale['402'] = "Resim Yönetimine Geri Dön";
+$locale['403'] = "Site Yönetimine Geri Dön";
 // Upload Image
-$locale['420'] = "Upload Image";
-$locale['421'] = "Filename:";
-$locale['422'] = "Main Images";
-$locale['423'] = "Article Images";
-$locale['424'] = "News Images";
-$locale['425'] = "Max. filesize %s / Allowed filetypes .jpg,.JPG,.png,.PNG,.JPEG,.gif,.GIF,.bmp,.BMP,.svg,.SVG,.tiff,.TIFF";
-$locale['426'] = "Image Uploaded";
-$locale['427'] = "News Cats Images";
-$locale['428'] = "Blog Images";
-$locale['429'] = "Blog Cats Images";
+$locale['420'] = "Resim Yükle";
+$locale['421'] = "Dosya İsmi:";
+$locale['422'] = "Ana Resimler";
+$locale['423'] = "İnceleme Resimleri";
+$locale['424'] = "Haber Resimleri";
+$locale['425'] = "Sadece gif, jpg ya da png formatında yükleme yapabilirsiniz.";
+$locale['426'] = "Resim Yüklendi";
+$locale['427'] = "Haber Kategorisi Resimleri";
+$locale['428'] = "Rütbe Resimleri";
+$locale['429'] = "Yüz İfadeleri";
 // View Image
-$locale['440'] = "View Image";
-$locale['441'] = "Unknown file format, cannot display image.";
-$locale['442'] = "Delete this image";
+$locale['440'] = "Resmi görüntüle";
+$locale['441'] = "Tanımlanamayan resim formatı, bu resim görüntülenemez.";
+$locale['442'] = "Bu resmi sil";
 // Image Management
-$locale['460'] = "Images";
-$locale['461'] = "View";
-$locale['462'] = "Delete";
-$locale['463'] = "This folder is empty";
-$locale['464'] = "Update image list for TinyMCE editor";
+$locale['460'] = "Resimler";
+$locale['461'] = "Göster";
+$locale['462'] = "Sil";
+$locale['463'] = "Bu klasör boş";
+$locale['464'] = "TinyMCE editorü için resim listesini güncelle";
 // Popup Error/Warning Messages
-$locale['470'] = "Delete this image?";
+$locale['470'] = "Bu Resim Silinsin mi?";
