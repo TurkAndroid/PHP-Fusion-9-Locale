6378d3a34d827e232fac3a007e18c4a67db2ee55
Türk Android <alper.peri@gmail.com>
2017-09-27 21:25:37 +0300

    Update blacklist.php

--- Turkish/locale/Turkish/admin/blacklist.php
+++ Turkish/locale/Turkish/admin/blacklist.php
@@ -1,43 +1,46 @@
 <?php
-$locale['BLS_000'] = "Blacklist"; //406
+/**
+ * @Translated by MAxWELL_TR Web Site(https://alperperi.com)
+ */
+$locale['BLS_000'] = "Kara liste"; //406
 //Blacklist message
-$locale['BLS_010'] = "Invalid E-mail or IP address.";
-$locale['BLS_011'] = "Adding users to blacklist.";
-$locale['BLS_012'] = "User Modified blacklist.";
-$locale['BLS_013'] = "User deleted from Blacklist"; //401
-$locale['BLS_014'] = "Are you sure you want to delete this entry?";
-$locale['BLS_015'] = "The blacklist is currently empty."; //465
-$locale['BLS_016'] = "Blacklist email address is not valid email."; //405
+$locale['BLS_010'] = "Geçersiz E-posta veya IP adresi.";
+$locale['BLS_011'] = "Kara listeye kullanıcılar ekleme.";
+$locale['BLS_012'] = "Değiştirilmiş Kullanıcı Kara Listesi.";
+$locale['BLS_013'] = "Kullanıcı Kara Listeden silindi"; //401
+$locale['BLS_014'] = "Bu girişi silmek istediğinizden emin misiniz?";
+$locale['BLS_015'] = "Kara liste şu an boş."; //465
+$locale['BLS_016'] = "Kara liste e-posta adresi geçerli bir e-posta değil."; //405
 
-$locale['BLS_020'] = "Blacklist User"; //420
-$locale['BLS_021'] = "Edit blacklisted user"; //421
-$locale['BLS_022'] = "Add blacklisted user";
-$locale['BLS_023'] = "Currently displaying %d of %d total Blacklist entries.";
+$locale['BLS_020'] = "Kara Liste Kullanıcısı"; //420
+$locale['BLS_021'] = "Kara listeye alınmış kullanıcıyı düzenle"; //421
+$locale['BLS_022'] = "Kara listeye kullanıcı ekle";
+$locale['BLS_023'] = "Şu anda %d - %d toplam kara liste girişleri görüntüleniyor.";
 
-$locale['BLS_030'] = "Blacklisted info"; //461
-$locale['BLS_031'] = "Admin"; //467
-$locale['BLS_032'] = "Date";  //468
-$locale['BLS_033'] = "Options"; //462
-$locale['BLS_034'] = "Blacklist IP address: [STRONG]or[/STRONG]"; //441
-$locale['BLS_035'] = "Blacklist email address"; //442
-$locale['BLS_036'] = "Blacklist reason"; //443
-$locale['BLS_037'] = "Blacklist user";  //444
-$locale['BLS_038'] = "Update";
+$locale['BLS_030'] = "Kara liste bilgisi"; //461
+$locale['BLS_031'] = "Yönetici"; //467
+$locale['BLS_032'] = "Tarih";  //468
+$locale['BLS_033'] = "Seçenekler"; //462
+$locale['BLS_034'] = "Kara liste IP adresi: [STRONG]or[/STRONG]"; //441
+$locale['BLS_035'] = "Kara liste e-posta adresi"; //442
+$locale['BLS_036'] = "Kara listeye neden olan sebep"; //443
+$locale['BLS_037'] = "Kara liste kullanıcısı";  //444
+$locale['BLS_038'] = "Güncelle";
 
-$locale['BLS_MS'] = "Entering an IP address will prevent a user whose IP address matches the entry from visiting this site.
-You can enter a full IP, e.g. <em>123.45.67.89.</em>, or a partial IP, e.g. <em>123.45.67</em> or <em>123.45</em>.
-Please note: IPv6 addresses are converted to their full length form on this site,
-e.g. <em>ABCD:1234:5:6:7:8:9:FF</em> will be shown as <em>ABCD:1234:0005:0006:0007:0008:0009:00FF</em>.
-Mixed IP addresses (those contain both IPv6 and IPv4 part) will not be checked for partial match.
+$locale['BLS_MS'] = "IP adresi girildiğinde, IP adresi aynı olan kullanıcının bu siteyi ziyaret etmesi engellenecektir.
+Tam bir IP girebilirsiniz, ör., <em>123.45.67.89.</em>, veya kısmi bir IP, ör. <em>123.45.67</em> veya <em>123.45</em>.
+Lütfen dikkat: IPv6 adresleri bu sitede tam uzunluk biçimine dönüştürülür,
+ör. <em>ABCD:1234:5:6:7:8:9:FF</em> olarak gösterilecek <em>ABCD:1234:0005:0006:0007:0008:0009:00FF</em>.
+Karışık IP adresleri (hem IPv6 hem de IPv4 kısmı içerenler) kısmi eşleşme olup olmadığı kontrol edilmeyecektir.
 <br /><br />
-Entering an email address will prevent members from registering using that address.
-You can enter a full email address, e.g. <em>foo@bar.com</em>, or an email domain, e.g. <em>bar.com</em>.<br /><br />
+Bir e-posta adresi girildiğinde, üyelerin o adresi kullanarak kayıt olmaları engellenir.
+Tam bir e-posta adresi girebilirsiniz, ör. <em>foo@bar.com</em>, veya bir e-posta ya da alan adı, ör. <em>bar.com</em>.<br /><br />
 
-% - matches any string.<br /><br />
+% - herhangi bir dizeye uyuyor.<br /><br />
 
-%.%.%.%@domain.tld bans any address that contains at least 3 dots.<br />
-%+%@domain.tld bans any address that contains at least one plus sign.<br />
-%@domain.tld bans any address from domain.tld<br />
-%.domain.tld bans all subdomains of domain.tld<br />
-%payday% bans any address that contains the word \"payday\" which was very often on sites.<br />
-domain.tld is an alias of %@domain.tld to make it compatible with rules defined in v7.<br />"; //440
+%.%.%.%@domain.tld en az 3 nokta içeren herhangi bir adresi yasaklar.<br />
+%+%@domain.tld en az bir artı işareti içeren herhangi bir adresi yasaklar.<br />
+%@domain.tld adresinden herhangi bir adresi yasaklar domain.tld<br />
+%.domain.tld tüm alt alanlarını yasaklaar domain.tld<br />
+%payday% sitelerde çok sık bulunan \"payday\" kelimesini içeren herhangi bir adresi yasaklar.<br />
+domain.tld v7'de tanımlanan kurallarla uyumlu hale getirmek için %@domain.tld'nin bir takma adıdır.<br />"; //440
